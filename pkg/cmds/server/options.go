256764a90a031a23516b2c190caf2fb8f2632046
Tamal Saha <tamal@appscode.com>
2018-04-14 10:00:24 -0700

    Increase qps and burst limits (#20)

--- pkg/cmds/server/options.go
+++ pkg/cmds/server/options.go
@@ -9,14 +9,22 @@ import (
 )
 
 type ControllerOptions struct {
+	QPS        float64
+	Burst      int
 	EnableRBAC bool
 }
 
 func NewControllerOptions() *ControllerOptions {
-	return &ControllerOptions{}
+	return &ControllerOptions{
+		QPS:   100,
+		Burst: 100,
+	}
 }
 
 func (s *ControllerOptions) AddGoFlags(fs *flag.FlagSet) {
+	fs.Float64Var(&s.QPS, "qps", s.QPS, "The maximum QPS to the master from this client")
+	fs.IntVar(&s.Burst, "burst", s.Burst, "The maximum burst for throttle")
+
 	fs.BoolVar(&s.EnableRBAC, "rbac", s.EnableRBAC, "Enable RBAC for operator")
 }
 
@@ -29,6 +37,8 @@ func (s *ControllerOptions) AddFlags(fs *pflag.FlagSet) {
 func (s *ControllerOptions) ApplyTo(cfg *controller.ControllerConfig) error {
 	var err error
 
+	cfg.ClientConfig.QPS = float32(s.QPS)
+	cfg.ClientConfig.Burst = s.Burst
 	cfg.EnableRBAC = s.EnableRBAC
 
 	if cfg.KubeClient, err = kubernetes.NewForConfig(cfg.ClientConfig); err != nil {
